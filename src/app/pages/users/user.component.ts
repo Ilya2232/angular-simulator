409a8b578cff8e473006e78dc495a59a0d1e8df6
Ilya06 <ilyagrigorov2006@gmail.com>
2026-08-19 18:07:42 +0500

    add cards

--- src/app/pages/users/user.component.ts
+++ src/app/pages/users/user.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { IUser } from '../../../interfaces/IUser';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
-export class UsersComponent { }
+export class UsersComponent implements OnInit {
+  public users$: Observable<IUser[]>;
+
+  constructor(private userService: UserService) {
+    this.users$ = this.userService.getUsers();
+  }
+
+  public ngOnInit(): void {
+    this.userService.loadUsers().subscribe();
+  }
+}
